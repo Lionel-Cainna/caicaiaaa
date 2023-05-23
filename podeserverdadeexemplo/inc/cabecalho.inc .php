44448efd07dc17b5cb92e6a74441fd487d23d1a5
Cainna Guxttavo <111900712+Lionel-Cainna@users.noreply.github.com>
2023-05-23 11:41:23 -0300

    Add files via upload

---
+++ podeserverdadeexemplo/inc/cabecalho.inc .php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PoDe SeR VeRdAdE</title>
+    <link rel="stylesheet" type="text/css" href="css/estilo.css">
+</head>
+<body>
+    <header>
+        <a href="index.php"><img src="img/podeserverdade.png" alt="Logo do PodeServerdade"></a>
+        <!---inicio sessao de busca--->
+        <h2>pesquisar<h2>
+        <session>
+            <form>
+                <imput type="search">
+                    <button>buscar</button>
+</form>
+</session>
+        <nav class="menu_opcoes">
+            <ul>
+                <li><a href="#">Fofocas</a></li>
+                <li><a href="#">Sua Conta</a></li>
+                <li><a href="#">Notícias</a></li>
+                <li><a href="#">SObre</a></li>
+                <li><a href="#">Ajuda</a></li>
+            </ul>
+        </nav>
+    </header>
